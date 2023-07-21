224b06f1d6be892007076f46e67420c893dee028
Mateusz Kozera <Matas.wp@gmail.com>
2023-07-21 15:37:24 +0200

    Update 11_load_uploaded.py

--- 11_load_uploaded.py
+++ 11_load_uploaded.py
@@ -10,6 +10,8 @@ from datetime import datetime
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)  # inicjacja aplikacji
+server = app.server
+
 
 app.layout = app.layout = html.Div([
     dcc.Upload(
