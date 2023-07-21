7e1c5e4b701758972154859a70f833b7b45f9fcc
Mateusz Kozera <Matas.wp@gmail.com>
2023-07-21 17:07:55 +0200

    Update csvread.py

--- csvread.py
+++ csvread.py
@@ -83,4 +83,4 @@ def update_output(list_of_contents , list_of_names , list_of_dates):
         return children
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
